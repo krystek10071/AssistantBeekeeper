fd36a370212ec6d7e75e3de78200bcc5058b733e
DESKTOP-FJU91NM\Ktoś <krystek10071@wp.pl>
2019-09-18 10:58:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/assistantbeekeeper/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.assistantbeekeeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
