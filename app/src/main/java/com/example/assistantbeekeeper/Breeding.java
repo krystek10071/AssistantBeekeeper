52650f6783b8511f045f126fb0044757d768ba7e
DESKTOP-FJU91NM\Ktoś <krystek10071@wp.pl>
2019-10-07 19:47:19 +0200

    add calendar View

--- app/src/main/java/com/example/assistantbeekeeper/Breeding.java
+++ app/src/main/java/com/example/assistantbeekeeper/Breeding.java
@@ -1,4 +1,15 @@
 package com.example.assistantbeekeeper;
 
-public class Breeding {
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Breeding extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.breeding);
+
+    }
 }
