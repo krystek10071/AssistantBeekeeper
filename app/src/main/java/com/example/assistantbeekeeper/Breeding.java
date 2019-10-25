5122151f8fa26c16a77e98ab8c99a31843c09e7c
DESKTOP-FJU91NM\Ktoś <krystek10071@wp.pl>
2019-10-25 21:55:32 +0200

    add calendar View and Events and eventslistView

--- app/src/main/java/com/example/assistantbeekeeper/Breeding.java
+++ app/src/main/java/com/example/assistantbeekeeper/Breeding.java
@@ -1,27 +1,77 @@
 package com.example.assistantbeekeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.github.sundeepk.compactcalendarview.domain.Event;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Breeding extends AppCompatActivity {
 
-    //CompactCalendarView compactCalendarView;
+    ListView eventsListView;
+    Date date=new Date();
+    Date helpDate=new Date();
+    ArrayList<String> listEvents=new ArrayList<>();
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breeding);
-        final CompactCalendarView compactCalendarView = (CompactCalendarView) findViewById(R.id.compact_calendar_view);
+
+        final CompactCalendarView compactCalendarView = findViewById(R.id.compact_calendar_view);
+        eventsListView= findViewById(R.id.events_listview);
+        date.setTime(1433701251000L);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         Event ev1 = new Event(Color.GREEN, 1433701251000L, "Some extra data that I want to store.");
         compactCalendarView.addEvent(ev1);
-        Event ev2 = new Event(Color.GREEN, 1571868000000L);
-        Event ev3 = new Event(Color.YELLOW, 1571868000000L);
-        compactCalendarView.addEvent(ev3);
-        compactCalendarView.addEvent(ev2);
+        listEvents.add(date.toString()  +  "Wywoz obornika");
+        addEvent(compactCalendarView, Color.GREEN, 1433701251000L , "Kolejne swieto nieudacznikow");
+        addEvent(compactCalendarView, Color.GREEN, 1433701251000L , "Kolejne swieto nieudacznikow");
+        addEvent(compactCalendarView, Color.GREEN, 1433701251000L , "Kolejne swieto nieudacznikow");
+        addEvent(compactCalendarView, Color.GREEN, 1433701251000L , "Kolejne swieto nieudacznikow");
+
+
+
+
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this, android.R.layout.simple_list_item_1, listEvents);
+        eventsListView.setAdapter(arrayAdapter);
 
     }
+
+
+    public void addEvent(CompactCalendarView compactCalendarView, int color , long timeInMillis, Object data ){
+            Event ev1=new Event(color, timeInMillis,data);
+            compactCalendarView.addEvent(ev1);
+            helpDate.setTime(timeInMillis);
+            listEvents.add(helpDate.toString() + data);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
