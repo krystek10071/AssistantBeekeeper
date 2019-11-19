584caa13f9f6677dc025522c34695c4931707ce6
DESKTOP-FJU91NM\Ktoś <krystek10071@wp.pl>
2019-11-19 12:13:40 +0100

    add class BreedingFunctions

--- app/src/main/java/com/example/assistantbeekeeper/Breeding.java
+++ app/src/main/java/com/example/assistantbeekeeper/Breeding.java
@@ -1,32 +1,22 @@
 package com.example.assistantbeekeeper;
 
-import android.content.ContentValues;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-
-import com.example.assistantbeekeeper.assistantbeekeepersqllite.FeedReaderContract;
 import com.example.assistantbeekeeper.assistantbeekeepersqllite.MyDbHandler;
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
-import com.github.sundeepk.compactcalendarview.domain.Event;
-
 import java.util.ArrayList;
-import java.util.Date;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Breeding extends AppCompatActivity {
 
     //declarations
     ListView eventsListView;
-    Date helpDate=new Date();
     ArrayList<String> listEvents=new ArrayList<>();
+    ArrayList<Long> listTimeInMillis=new ArrayList<>();
+    ArrayList<String> listDescription=new ArrayList<>();
     Button addBreedingButton;
     ArrayAdapter arrayAdapter;
 
@@ -38,52 +28,30 @@ public class Breeding extends AppCompatActivity {
         final CompactCalendarView compactCalendarView = findViewById(R.id.compact_calendar_view);
 
     //fields
+        final MyDbHandler dbHelper=new MyDbHandler(this);
+        final BreedingFunctions breedingFunctions=new BreedingFunctions();
         eventsListView= findViewById(R.id.events_listview);
         addBreedingButton=findViewById(R.id.add_breeding_button);
         arrayAdapter=new ArrayAdapter(this, android.R.layout.simple_list_item_1, listEvents);
-        final MyDbHandler dbHelper=new MyDbHandler(this);
+
+        breedingFunctions.addTimeMillisToList(listTimeInMillis, listDescription);                                //add Date in millis and description to Lists
 
 
-
-
-
-        ////////////////////////////////////////Listeners/////////////////////////////////////////////////////
-        ////////////////////////////////////Buttons Add Breeding/////////////////////////////////////////////////
+        ////////////////////////////////////Buttons Add Breeding////////////////////////////////////
 
         addBreedingButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-               SQLiteDatabase db=dbHelper.getWritableDatabase();
-                addBreeding(compactCalendarView, dbHelper ,1572303600000L);
-                 eventsListView.setAdapter(arrayAdapter);
+                breedingFunctions.addBreeding(compactCalendarView, listTimeInMillis, listDescription, listEvents);
+                eventsListView.setAdapter(arrayAdapter);
+                breedingFunctions.addToDatabase(dbHelper,listTimeInMillis, listDescription);
+
             }
         }
         );
     }
 
 
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
