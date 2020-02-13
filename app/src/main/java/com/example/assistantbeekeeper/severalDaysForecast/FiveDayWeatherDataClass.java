52f0cf01e0c2b61ac841cbc7f9dc54f478648243
DESKTOP-FJU91NM\Ktoś <krystek10071@wp.pl>
2020-02-13 20:56:46 +0100

    Add FiveWeatherDataClass

--- app/src/main/java/com/example/assistantbeekeeper/severalDaysForecast/FiveDayWeatherDataClass.java
+++ app/src/main/java/com/example/assistantbeekeeper/severalDaysForecast/FiveDayWeatherDataClass.java
@@ -2,12 +2,69 @@ package com.example.assistantbeekeeper.severalDaysForecast;
 
 import com.example.assistantbeekeeper.weatherwitget.WeatherDataGeneral;
 
-public class FiveDayWeatherDataClass extends WeatherDataGeneral {
+public class FiveDayWeatherDataClass  {
 
+    private long epochDate;
+    private int weatherIcon;
+    private String iconPhrase;
+    private double tDay;
+    private double tNight;
+    private double windSpeed;
 
-    public FiveDayWeatherDataClass( long epochDate, String location, int whetherIcon, String iconPhrase, double temperature,
-                                   double realFeelTemperature, double windSpeed, int precipitationProbality) {
-        super( epochDate, location, whetherIcon, iconPhrase, temperature,
-                realFeelTemperature, windSpeed, precipitationProbality);
+    public FiveDayWeatherDataClass(long epochDate, int weatherIcon, String iconPhrase, double tDay, double tNight, double windSpeed) {
+        this.epochDate = epochDate;
+        this.weatherIcon = weatherIcon;
+        this.iconPhrase = iconPhrase;
+        this.tDay = tDay;
+        this.tNight = tNight;
+        this.windSpeed = windSpeed;
+    }
+
+    public long getEpochDate() {
+        return epochDate;
+    }
+
+    public void setEpochDate(long epochDate) {
+        this.epochDate = epochDate;
+    }
+
+    public int getWheatherIcon() {
+        return weatherIcon;
+    }
+
+    public void setWheatherIcon(int wheatherIcon) {
+        this.weatherIcon = wheatherIcon;
+    }
+
+    public String getIconPhrase() {
+        return iconPhrase;
+    }
+
+    public void setIconPhrase(String iconPhrase) {
+        this.iconPhrase = iconPhrase;
+    }
+
+    public double gettDay() {
+        return tDay;
+    }
+
+    public void settDay(double tDay) {
+        this.tDay = tDay;
+    }
+
+    public double gettNight() {
+        return tNight;
+    }
+
+    public void settNight(double tNight) {
+        this.tNight = tNight;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
     }
 }
--- app/src/main/java/com/example/assistantbeekeeper/severalDaysForecast/ParseJson.java
+++ app/src/main/java/com/example/assistantbeekeeper/severalDaysForecast/ParseJson.java
@@ -1,17 +1,47 @@
 package com.example.assistantbeekeeper.severalDaysForecast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class ParseJson {
 
     private String resultData;
-    private ArrayList<FiveDayWeatherDataClass> fiveDaysWeatherDataObj;
 
       ParseJson(String string){
          this.resultData=string;
      }
 
-     public ArrayList<FiveDayWeatherDataClass> parseJsonToObjectList(){
+     public ArrayList<FiveDayWeatherDataClass> parseJsonToObjectList(ArrayList<FiveDayWeatherDataClass> weatherDataObj){
+
+          if(weatherDataObj!=null){
+              weatherDataObj.clear();
+          }
+
+          if(resultData!=null){
+            try {
+                int lengthDailyForecast;
+                JSONObject jsonObject=new JSONObject(resultData);
+                JSONArray dailyForecastArray=jsonObject.getJSONArray("DailyForecasts");
+                lengthDailyForecast=dailyForecastArray.length();
+
+                for(int i=0; i<lengthDailyForecast; i++){
+                    JSONObject resultObj=dailyForecastArray.getJSONObject(i);
+                    long epochDate=resultObj.getLong("EpochDate");
+                    int whetherIcon=resultObj.getJSONObject("Day").getInt("Icon");
+                    String iconPhrase=resultObj.getJSONObject("Day").getString("IconPhrase");
+                    double tDay=resultObj.getJSONObject("Temperature").getJSONObject("Maximum").getDouble("Value");
+                    double tNight=resultObj.getJSONObject("Temperature").getJSONObject("Minimum").getDouble("Value");
+                    double windSpeed=resultObj.getJSONObject("Day").getJSONObject("Wind").getJSONObject("Speed").getDouble("Value");
+
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+          }
+
 
          return null;
      }
