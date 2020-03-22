a1f29485a1cd0f56fe94e44674c50f291c58d138
DESKTOP-FJU91NM\Ktoś <krystek10071@wp.pl>
2020-03-22 22:53:13 +0100

    add form cost xml layout

--- app/src/main/java/com/example/assistantbeekeeper/panelButtonFragment/panelButtonActivity/EntryFormActivity.java
+++ app/src/main/java/com/example/assistantbeekeeper/panelButtonFragment/panelButtonActivity/EntryFormActivity.java
@@ -1,33 +1,68 @@
 package com.example.assistantbeekeeper.panelButtonFragment.panelButtonActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.assistantbeekeeper.BreedingFunctions;
 import com.example.assistantbeekeeper.R;
-import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
 
-import java.util.Objects;
+import java.util.Calendar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EntryFormActivity extends AppCompatActivity {
-    TextInputLayout textInputLayout;
-    TextInputLayout textInputLayout2;
+    TextInputLayout textInputName;
+    TextInputLayout textInputValue;
+    EditText textInputDate;
+    Button buttonAkcept;
+    FloatingActionButton floatingActionButton;
+    private Calendar calendar;
+    Long timeInMillis;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form_cost_profit);
         init();
-        //String string= Objects.requireNonNull(textInputLayout.getEditText()).getText().toString().trim();
 
+        floatingActionButton.setOnClickListener(view -> { setDateWithDataPicker(this);});
     }
 
     public void init(){
-        textInputLayout=findViewById(R.id.textInput1);
-        textInputLayout2=findViewById(R.id.textInput2);
+        textInputName=findViewById(R.id.textInput1);
+        textInputValue=findViewById(R.id.textInput2);
+        textInputDate=findViewById(R.id.textInput3);
+        buttonAkcept=findViewById(R.id.buttonAkcept);
+        floatingActionButton=findViewById(R.id.floatingActionButton1);
+    }
+
+    private void setDateWithDataPicker(Context context){
+        DatePickerDialog datePickerDialog;
+        calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        final int month = calendar.get(Calendar.MONTH);
+        int year = calendar.get(Calendar.YEAR);
+        // final Long[] timeinmillis = new Long[1];
+        //timeinmillis[0]=0L;
+        //set breeding day with DatePickerDialog
+        datePickerDialog = new DatePickerDialog(context, (datePicker, mYear, mMonth, mDay) -> {
+            calendar.set(mYear, mMonth, mDay);
+            calendar.set(Calendar.MINUTE, 0);
+            calendar.set(Calendar.SECOND, 0);
+            calendar.set(Calendar.HOUR_OF_DAY, 0);
+            timeInMillis = calendar.getTimeInMillis();
+            Log.i("WYBOR DATY", Long.toString(timeInMillis));
+            String output=String.format("%tQ", calendar.getTimeInMillis());                     //set output string with time in millis
+        }, day, month, year);
+        datePickerDialog.show();
     }
 }
