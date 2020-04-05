6a7ff3d46c8865712e2c603a6947e57aa33f2756
DESKTOP-FJU91NM\Ktoś <krystek10071@wp.pl>
2020-04-05 10:33:22 +0200

    add recyleview_list_of_operations.xml

---
+++ app/src/main/java/com/example/assistantbeekeeper/statisticsProduction/detailStatistic/detailStatisticActivity/AdapterDetailStatistics.java
@@ -0,0 +1,24 @@
+package com.example.assistantbeekeeper.statisticsProduction.detailStatistic.detailStatisticActivity;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterDetailStatistics extends RecyclerView.Adapter<ViewHolderDetailStatistics> {
+    @NonNull
+    @Override
+    public ViewHolderDetailStatistics onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderDetailStatistics holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/assistantbeekeeper/statisticsProduction/detailStatistic/detailStatisticActivity/ViewHolderDetailStatistics.java
@@ -0,0 +1,15 @@
+package com.example.assistantbeekeeper.statisticsProduction.detailStatistic.detailStatisticActivity;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolderDetailStatistics extends RecyclerView.ViewHolder {
+    public View view;
+
+    public ViewHolderDetailStatistics(View view) {
+        super(view);
+        this.view = view;
+    }
+}
